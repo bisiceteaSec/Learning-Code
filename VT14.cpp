3814457e89de557854bf82331a89e72b97479e96
bisicetea <bisiceteasec@gmail.com>
2022-09-07 23:45:25 +0700

    7/9

---
+++ VT14.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a, a + n);
+    int f = a[0] * a[1];
+    int l = a[n - 1] * a[n - 2];
+
+    cout << max(f, l);
+}
