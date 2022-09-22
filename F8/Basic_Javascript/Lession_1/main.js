14b001a70b18ae3919e6311f47cb388f975e0b4c
bisicetea <bisiceteasec@gmail.com>
2022-09-22 17:05:10 +0700

    JS Day 1

---
+++ F8/Basic_Javascript/Lession_1/main.js
@@ -0,0 +1,20 @@
+var fullName = "Tran Van Nhan";
+var age = 123;
+
+// alert(fullName);
+// alert(age);
+
+
+// console.log(fullName);
+
+// confirm('Enough age?');
+
+// prompt('Your age:');
+
+// setTimeout(function () {
+//     alert('Notification!')
+// }, 1000);
+
+// setInterval(function () {
+//     console.log('This is log!' + Math.random())
+// }, 1000)
