9bf1801ba88c4af69793e614bd4489d2e78d49da
bisicetea <bisiceteasec@gmail.com>
2022-09-23 00:28:42 +0700

    Lession_3

---
+++ F8/Basic_Javascript/Lession_3/main.js
@@ -0,0 +1,29 @@
+// var a = 1 + 2;
+// console.log(a);
+
+// var fullName = 'Tran Van Nhan';
+// var a = 1, b = 2;
+
+// if (a == b)
+//     alert('Correct!')
+// else
+//     alert('Wrong!')
+
+// if (a > 0 && b > 0)
+//     alert('a > 0 and b > 0')
+// else
+//     alert('bruh!')
+
+// var c = a + b;
+// console.log(c)
+
+// var firstName = 'Van';
+// var lastName = 'Nhan';
+
+// console.log(firstName + ' ' + lastName)
+
+
+var id = Symbol('id');
+var id2 = Symbol('id');
+
+console.log(id, id2);
